46923dd6433e7137203993f46b8a3e1621e9b52e
Lennart Klüner <l_kluener@gmx.de>
2021-03-03 15:40:26 +0100

    alles eingetragen_Pins etc

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,12 @@ int wt1 = 11; //Pinbelegung Weichentasten
 int wt2 = 20;
 int wt3 = 2;
 
-int ledw1g = 102; //Pinbelegung der WeichenLEDs
-int ledw1k = 103;
-int ledw2g = 104;
-int ledw2k = 105;
-int ledw3g = 104;
-int ledw3k = 105;
+int ledw1g = 109; //Pinbelegung der WeichenLEDs
+int ledw1k = 108;
+int ledw2g = 50;
+int ledw2k = 39;
+int ledw3g = 106;
+int ledw3k = 104;
 
 int weichentimeout = 1000; //dauer des Schaltvorgangs bei den Weichen
 int wgt = 18;              //Weichengruppentaste
@@ -39,7 +39,7 @@ int zta8 = 4;
 int zta9 = 7;
 
 //Melder
-int ftueMelderLed = 101; //Fahrstraßentastenüberwachung
+int ftueMelderLed = 124; //Fahrstraßentastenüberwachung
 int ftueMelderWut = 19;
 int weckerPin = 28;
 String ftueMelderName = "ftueMelder";
@@ -51,111 +51,119 @@ int shgt = 15;                                                          //s.o
 int allgSignaltasten[3] = {signalsperrtaste, Signalentsperrtaste, shgt}; //Signalsperrtaste, Signalentsperrtaste, Signalhaltgruppentaste, Array speichert für jedes Signal wichtige informationen, hält den schreibaufwand geringer
 
 //Signal1
-int rot1 = 106;   //Hp0 (rot) vom Signal 1
-int gruen1 = 107; //Hp1 (grün)  vom Signal 1
-int gelb1 = 108;  //Hp2 (langsamfahrt) vom Signal 1
-int sperrmelder1 = 109;
+int rot1 ;   //Hp0 (rot) vom Signal 1
+int gruen1 = 126; //Hp1 (grün)  vom Signal 1
+int gelb1;  //Hp2 (langsamfahrt) vom Signal 1
+int sperrmelder1 = 116;
 int signaltaste1 = zta3; //Signaltaste, zu tastzwecken weichentaste 1
 
 //Signal2
-int rot2 = 106;   //Hp0 (rot) vom Signal 1
-int gruen2 = 107; //Hp1 (grün)  vom Signal 1
-int gelb2 = 108;  //Hp2 (langsamfahrt) vom Signal 1
-int sperrmelder2 = 109;
-int signaltaste2 = zta3; //Signaltaste, zu tastzwecken weichentaste 1
+int rot2 = 123;   //Hp0 (rot) vom Signal 1
+int gruen2 = 46; //Hp1 (grün)  vom Signal 1
+int gelb2;  //Hp2 (langsamfahrt) vom Signal 1
+int sperrmelder2 = 49;
+int signaltaste2 = zta4; //Signaltaste, zu tastzwecken weichentaste 1
 
 //Signal3
-int rot3 = 106;   //Hp0 (rot) vom Signal 1
-int gruen3 = 107; //Hp1 (grün)  vom Signal 1
-int gelb3 = 108;  //Hp2 (langsamfahrt) vom Signal 1
-int sperrmelder3 = 109;
-int signaltaste3 = zta3; //Signaltaste, zu tastzwecken weichentaste 1
+int rot3 = 121;   //Hp0 (rot) vom Signal 1
+int gruen3 = 122; //Hp1 (grün)  vom Signal 1
+int gelb3;  //Hp2 (langsamfahrt) vom Signal 1
+int sperrmelder3 = 127;
+int signaltaste3 = zta7; //Signaltaste, zu tastzwecken weichentaste 1
 
 //Signal4
-int rot4 = 106;   //Hp0 (rot) vom Signal 1
-int gruen4 = 107; //Hp1 (grün)  vom Signal 1
-int gelb4 = 108;  //Hp2 (langsamfahrt) vom Signal 1
-int sperrmelder4 = 109;
-int signaltaste4 = zta3; //Signaltaste, zu tastzwecken weichentaste 1
+int rot4;   //Hp0 (rot) vom Signal 1
+int gruen4 = 111; //Hp1 (grün)  vom Signal 1
+int gelb4;  //Hp2 (langsamfahrt) vom Signal 1
+int sperrmelder4 = 48;
+int signaltaste4 = zta8; //Signaltaste, zu tastzwecken weichentaste 1
 
-const int hauptsignalanzahl = 1;
-int rot[hauptsignalanzahl] = {rot1};
-int gelb[hauptsignalanzahl] = {gelb1};
-int gruen[hauptsignalanzahl] = {gruen1};
-int signaltasten[hauptsignalanzahl] = {signaltaste1};
-int sperrmelder[hauptsignalanzahl] = {sperrmelder1};
+const int hauptsignalanzahl = 4;
+int rot[hauptsignalanzahl] = {rot1, rot2, rot3, rot4};
+int gelb[hauptsignalanzahl] = {gelb1, gelb2, gelb3, gelb4};
+int gruen[hauptsignalanzahl] = {gruen1, gruen2, gruen3, gruen4};
+int signaltasten[hauptsignalanzahl] = {signaltaste1, signaltaste2, signaltaste3, signaltaste4};
+int sperrmelder[hauptsignalanzahl] = {sperrmelder1, sperrmelder2, sperrmelder3, sperrmelder4};
 HauptsignalControl hauptsignale(hauptsignalanzahl, rot, gelb, gruen, signaltasten, sperrmelder, allgSignaltasten, schieberegisterPins);
 //int rotPin, int gelbPin, int gruenPin,  int signaltaste, int sperrmelder, int allgSignaltasten[3], int registerPin[4]
 
 //Gleisbesetztmelder
-const int besetztmelderAnzahl = 6;
-int weichenbesetztmelder[besetztmelderAnzahl] ={0,2,0,0,1,0};//übergeben der Weichennr
-int besetztmelderEingaenge[besetztmelderAnzahl] = {42, 41, 40, 39, 38, 37}; //an gnd angeschlossen
-int besetztmelderLedsGelb[besetztmelderAnzahl] = {0, 0, 0, 110, 0, 112};
-int besetztmelderLedsRot[besetztmelderAnzahl] = {0, 0, ftueMelderLed, 111, 0, 113};
+//jedes Feld muss als einzelner Gleisbesetztmelder definiert sein
+const int besetztmelderAnzahl = 19;
+int weichenbesetztmelder[besetztmelderAnzahl] ={0,0,
+                                                0,0, 0, 0,1, 0,2, 0,0,
+                                                0,0, 0,0, 3,0, 0,0};//Weichennummern werden übergeben
+int besetztmelderEingaenge[besetztmelderAnzahl] = {69,69,59,59,63,65,65,68,68,67,67,60,60,64,64,62,62,61,61}; //an gnd angeschlossen
+int besetztmelderLedsGelb[besetztmelderAnzahl] = {33,45,130,116,117,114,0,102,0,31,43,130,128,119,112,0,37,35,41};
+int besetztmelderLedsRot[besetztmelderAnzahl] = {32,44,132,0,118,115,0,103,30,42,131,129,120,113,0,36,34,40};
 BesetztmeldungControl besetztmeldung(besetztmelderEingaenge, besetztmelderLedsGelb, besetztmelderLedsRot, weichenbesetztmelder, besetztmelderAnzahl, schieberegisterPins);
 
 //Weichen
-const int anzahlWeichen = 2;
-int weichenPinsGerade[anzahlWeichen] = {w1g, w2g};
-int weichenPinsKurve[anzahlWeichen] = {w1k, w2k};
-int weichenLedPinsGerade[anzahlWeichen] = {ledw1g, ledw2g};
-int weichenLedPinsGeradeRot[anzahlWeichen] = {ftueMelderLed, 111};
-int weichenLedPinsKurve[anzahlWeichen] = {ledw1k, ledw2k};
-int weichenLedPinsKurveRot[anzahlWeichen] = {};
-int adressWeichenpositionen[anzahlWeichen] = {1, 5};
-int weichentasten[anzahlWeichen] = {wt1, wt2};
+const int anzahlWeichen = 3;
+int weichenPinsGerade[anzahlWeichen] = {w1g, w2g, w3g};
+int weichenPinsKurve[anzahlWeichen] = {w1k, w2k, w3k};
+int weichenLedPinsGerade[anzahlWeichen] = {ledw1g, ledw2g, ledw3g};
+int weichenLedPinsGeradeRot[anzahlWeichen] = {110,101,107};
+int weichenLedPinsKurve[anzahlWeichen] = {ledw1k, ledw2k, ledw3k};
+int weichenLedPinsKurveRot[anzahlWeichen] = {0,38,105};
+int adressWeichenpositionen[anzahlWeichen] = {1, 5, 9};
+int weichentasten[anzahlWeichen] = {wt1, wt2, wt3};
 WeichenControl weichen(anzahlWeichen, weichenPinsGerade, weichenPinsKurve, weichenLedPinsGerade, weichenLedPinsGeradeRot, weichenLedPinsKurve, weichenLedPinsKurveRot, adressWeichenpositionen, weichentimeout, weichentasten, wgt, schieberegisterPins);
 
 //Melder
 Melder ftueMelder(ftueMelderName, ftueMelderLed, weckerPin, ftueMelderWut, schieberegisterPins); //Ausgabe von FTÜ im Seriellen Monitior funktioniert nicht
 
 //Zugtasten
-const int zugtastenanzahl = 4;
-int zugtastenPins[zugtastenanzahl] = {zta1, zta2, zta3, zta4};
+const int zugtastenanzahl = 9;
+int zugtastenPins[zugtastenanzahl] = {zta1, zta2, zta3, zta4, zta5, zta6, zta7, zta8,zta9};
 ZugtastenControl zugtastenC(zugtastenanzahl, zugtastenPins);
 
 //Fahrstraßensteuerung
-const int fahrstrassenanzahl = 3;
+const int fahrstrassenanzahl = 8;
 int zugtastenspeicher[2];                      //es werden die Zugtasten gespeichert, die gedrückt wurden.
 int anzahl = 0;                                //zählt wie viele zugtasten gedrückt wurden
 boolean fahrstrassenstati[fahrstrassenanzahl]; //Beinhaltet, welche Fahrstrassen aktiv sind.
 boolean fahrstrassenkontolle[fahrstrassenanzahl];//wenn auf true, dann sind alle Besetztmelder frei und für die Fahrstrasse festgelegt und die Stellaufträge für die Weichen sind gegeben.
-int const felderAnzahl = 18;
+int const felderAnzahl = 9;//Anzahl der Felder auf dem Feld
 int fahrstrasse;                                      //zeigt an welche Fahrstrasse kurzzeitig aktiv ist.
 int fahrstrassenspeicher[felderAnzahl][felderAnzahl]; //1 variable = nummer des Tischfeldes, 2. variable = nummer des Zweiten Tischfeldes ergibt die zu betätigende Fahrstrasse
-int freigabe[fahrstrassenanzahl] = {1, 1, 1};         //zählt wie viele halbe Besetztmelder schon freigegeben wurden
-int besetztmelderposition[fahrstrassenanzahl + 1][/*Reihen(Bestztm.*/ 5][5] = {
+int freigabe[fahrstrassenanzahl] = {1,1,1,1,1,1,1,1};         //zählt wie viele halbe Besetztmelder schon freigegeben wurden
+int besetztmelderposition[fahrstrassenanzahl + 1][/*Reihen(Bestztm.)*/ 9][5/*Tiefe*/] = {
     {{}},
-    {{3, 1}, {3}, {4, 1, 1}, {5}},
-    {{4, 1}, {3}, {4, 1, 0}, {1, 2, 0}, {2}},
-    {{3, 1}, {0}, {1, 2, 1}, {2}},
+    {{7, 1}, {4}, {5}, {6,1,1}, {7}, {8,2,1}, {9}, {10}},//3-5 ->
+    {{7,1}, {4}, {5}, {6,1,1}, {7}, {8,2,0}, {0}, {1}},//3-1 ->
+    {{7,1}, {4}, {5}, {6,1,0},{15,3,0}, {16}, {17}, {18}},//3-9 ->
+    {{6,3}, {13}, {14}, {15,3,1}, {16}, {17}, {18}},//7-9 ->
+    {{7,2}, {8,2,1}, {7}, {6,1,1}, {5}, {4}, {3}, {2}},//4-2 <-
+    {{6,4}, {16}, {15,3,1}, {14}, {13}, {12}, {11}},//8-6 <-
+    {{7,4}, {16}, {15,3,0}, {6,1,0}, {5}, {4}, {3}, {2}},//8-2 <-
+    {{7,0}, {8,2,0}, {7}, {6,1,1}, {5}, {4}, {3}, {2}}//1-2 <-
 };                                               //0. ebene Besetztmelder anzahl, in den nächsten ebenen sind die Besetztmelder in Reihenfolge gespeichert. Die nächste Dimeinsion speichert, ob das Feld auf dem Besetztmelder eine Weiche ist, und darunter steht in welche richtung die geschaltet werden muss. Folgend steht, ob es eine Falnkenschutz weiche gibt                                                                            //anhand dieser Zahl lässt sich der Wert von Freigabe berechnen, enthält bis zu welchem Besetztmelder die Fahrstrasse aufgelöst ist.
 int besetztmelderzahl[fahrstrassenanzahl] = {0}; //die anzahl der Besetztmelder die schon freigegeben wurde
 
 
 void setup()
 {
-  /*weichen.weichenRelaisHIGH();       //alle möglichen Eingaben an den Relais werden gelöscht
+  weichen.weichenRelaisHIGH();       //alle möglichen Eingaben an den Relais werden gelöscht
   weichen.weichenpositionenEEPROM(); //gespeicherte Weichenposition wird angezeigt und ausgeführt
   Serial.begin(600);
 
   //Fahrstrassentastenkombinationen
-  fahrstrassenspeicher[2][3] = 1;
-  fahrstrassenspeicher[1][2] = 2; 
-  fahrstrassenspeicher[0][1] = 3;//wenn die Tasten auf feld 10 und feld 8 gedrückt werden, soll die Fahrstraße 1 einlaufen
-*/
+  fahrstrassenspeicher[3][5] = 1;
+  fahrstrassenspeicher[1][3] = 2; 
+  fahrstrassenspeicher[3][9] = 3;
+  fahrstrassenspeicher[7][9] = 4;
+  fahrstrassenspeicher[2][4] = 5;
+  fahrstrassenspeicher[6][8] = 6;
+  fahrstrassenspeicher[2][8] = 7;
+  fahrstrassenspeicher[1][2] = 8;//wenn die Tasten auf feld 1 und feld 2 gedrückt werden, soll die Fahrstraße 8 einlaufen
+
 
 }
 
 void loop()
 {
-  int k = 133;
-  ftueMelder.digitalSchalten(k, HIGH);
-  delay(1000);
-  ftueMelder.digitalSchalten(k, LOW);
-  delay(1000);
-  /*
+  
   //abfragen aller Zugtasten
   zugtastenspeicher[0] = 0;
   zugtastenspeicher[1] = 0;
@@ -227,7 +235,7 @@ void loop()
 
     if (fahrstrassenstati[kontrollen - 1] == true)
     {
-      //auf einen Besetztmelder sind immer zwei zahlen festgelegt, die erste Zahl wird beim besetztsein des melder eingespeichert, die zweite, wird erst dnn eingespeichert, wenn die erste eingespeichert ist,
+      //auf einen Besetztmelder sind immer zwei zahlen festgelegt, die erste Zahl wird beim besetztsein des melder eingespeichert, die zweite, wird erst dann eingespeichert, wenn die erste eingespeichert ist,
       //der nächste besetztmelder kann erst freigegeben werden, wenn der erste freigegeben ist, also die Zahl eine bestimmte höhe hat.
       if (besetztmeldung.getBesetztmelderstatus(besetztmelderposition[kontrollen][besetztmelderzahl[kontrollen - 1] + 1][0], LOW, weichen) == HIGH) //wenn der Besetztmelder besetzt ist und die freigabe der Besetztmelder Zahl plus eins entspricht
       {
@@ -269,12 +277,20 @@ void loop()
   weichen.weichenSchalten(); //lässt das Relais nach dem Schalten wieder zurückfallen
 
   //Besetztmeldung
-  //besetztmeldung.besetztmelderAusfuehren();//besetztmelder sollen immer wenn sie belegt sind den Status anzeigen.
-  besetztmeldung.getBesetztmelderstatus(0, LOW, weichen);
-  besetztmeldung.getBesetztmelderstatus(1, LOW, weichen);
+  //besetztmelder sollen immer wenn sie belegt sind den Status anzeigen.
+  for (size_t h = 0; h < 18; h++)
+  {
+    besetztmeldung.getBesetztmelderstatus(h, LOW, weichen);
+  }
+  
+  /*besetztmeldung.getBesetztmelderstatus(1, LOW, weichen);
   besetztmeldung.getBesetztmelderstatus(2, LOW, weichen);
   besetztmeldung.getBesetztmelderstatus(3, LOW, weichen);
   besetztmeldung.getBesetztmelderstatus(4, LOW, weichen);
   besetztmeldung.getBesetztmelderstatus(5, LOW, weichen);
+  besetztmeldung.getBesetztmelderstatus(6, LOW, weichen);
+  besetztmeldung.getBesetztmelderstatus(7, LOW, weichen);
+  besetztmeldung.getBesetztmelderstatus(8, LOW, weichen);
+  besetztmeldung.getBesetztmelderstatus(9, LOW, weichen);
 */
 }
